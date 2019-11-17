2e96567521103e0e5b54a0a906e342d20f1ac21e
Johnathan Machler <machl005@umn.edu>
2019-11-17 00:48:43 -0600

    Add files via upload

---
+++ js/jumble-up-text.js
@@ -0,0 +1,5 @@
+$("a.jumble-up-text").on("mouseover mouseout", function(){
+    var $this = $(this);
+    var sShowTxt = $this.data('changetxt');
+    $this.data('changetxt', $this.text());   
+    $this.text(sShowTxt);
